49bafb6a9b37fec9b062ee8eeffc098a4fdceb9b
Bajan <valleyne+github@gmail.com>
2020-11-05 17:10:07 -0500

    Updates

---
+++ precache-manifest.d91f720abc193f546a5c9e0b0e098763.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6331c80640d51d3a04a35bb28b84497",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7d65e0d887d1a91fcd1f",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fca51abfb1645adc169c",
+    "url": "/robofriends/static/css/main.98de6ebc.chunk.css"
+  },
+  {
+    "revision": "7d65e0d887d1a91fcd1f",
+    "url": "/robofriends/static/js/2.b52811a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.b52811a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca51abfb1645adc169c",
+    "url": "/robofriends/static/js/main.12332175.chunk.js"
+  },
+  {
+    "revision": "11eb4e7e2bc4c8587018",
+    "url": "/robofriends/static/js/runtime-main.a0a4b82b.js"
+  }
+]);
